2281f2db36e36c9e41fb250c8c1bba7a89a2079a
azimergashov <azimboy.ergashov.1@gmail.com>
2022-05-21 23:37:24 +0500

    5-dars 2-masala

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const mark=Number(prompt(`Markning massasini kiriting:`))
+
+const markHeight=Number(prompt(`Markning boyini kiriting:`))
+
+
+console.log(`Markning BMIsi-${ mark/markHeight**2}`);
+
+const john=Number(prompt(`Johnni massasini kiriting:`))
+
+const johnHeight=Number(prompt(`Johnni boyini kiriting:`))
+
+
+console.log(`Johnni BMIsi-${john/johnHeight**2}`);
